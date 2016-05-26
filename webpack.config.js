b3f95985b0768e8dc95d2954f62d220152e87a73
SaanGithub <saanrashid@hotmail.com>
2016-05-26 11:20:36 +0200

    new webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,8 @@ module.exports = {
       {test: /\.coffee$/, exclude: /node_modules/, loader: "coffee-loader"}
     ]
   },
+  output: {
+    filename: "index_bundle.js",
+    path: __dirname + '/dist'
+  },
 }
